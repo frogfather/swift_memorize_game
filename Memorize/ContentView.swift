5d6c2a588ed785bf45e085604a1914e80d556fec
frogfather <j.a.campbell@icloud.com>
2022-03-19 10:44:51 +0000

    Up to end of lecture 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["✈️","🚴🏻‍♂️","🚂","🚢"]
+    @State var emojiCount = 4
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+        VStack {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+            ForEach(emojis[0..<emojiCount], id: \.self, content: { emoji in
+                CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+            })
+            
+        }.foregroundColor(.red)
+            Spacer()
+        }.padding(.horizontal)
     }
 }
+
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    var body: some View {
+        ZStack{
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if (isFaceUp) {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill().foregroundColor(.red)
+            }
+            
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
+
